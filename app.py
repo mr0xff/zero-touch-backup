15bc0da13c1331e23d5d0fbebf0f9f99dbed1b84
0xff <worktrabalho633@gmail.com>
2025-12-02 23:40:25 +0100

    add: test app.py file and message module

---
+++ app.py
@@ -0,0 +1,5 @@
+from utls.logmod import LodModule
+
+if __name__ == "__main__":
+  a = LodModule("log.txt")
+  
--- utls/logmod.py
+++ utls/logmod.py
@@ -1,13 +1,25 @@
-class LogMode: 
-  def __init__(self): 
-    pass
+class LodModule: 
   
+  def __init__(self, filename, stdout = False): 
+    self.filename = filename
+    self.stdout = stdout
+    self.__open_file()
+  
+  def __open_file(self):
+    # fazer a abertura do arquivo e os devidos teste se o arquivo existe...
+    print(f"open file {self.filename}")
 
-  def info():
+  def info(msg):
     pass
   
   def warn(): 
     pass
-  
+
   def error():
     pass
+
+class MessageLog: 
+  def __init__(self, message):
+    self.mesage = message
+  
+
